corrected Rn formula from paper

src/dmg_hr/bart.py
@@ -43,7 +43,7 @@ def neck_resistance(mu, gamma, thcon, cp, nl, nr, rho, afreq):
     return rn
 
 def neck_resistance_paper(nl, nr, mu, rho, afreq):
-    rn = (2 * (nl / nr)) * (sqrt(2 * mu * rho * afreq) / (np.pi * nr ** 2))
+    rn = (sqrt(2 * rho * afreq * mu ) / (np.pi * nr ** 2)) * (2 + (nl / nr))  
     return rn
 
 
@@ -91,10 +91,10 @@ if __name__ == '__main__':
         m = mn + mb                                     # total mass (kg/m3)
         vol = (sb * bl)# (sn * nl) + (sb * bl)                     # volume (m3)
 
-        rn = neck_resistance(mu, gamma, thcon, cp, nl, nr, rho, afreq)  # neck resitance (kg/s)
         #TODO neck resistance values still differ slightly from Barts chart
-        # rn = neck_resistance_paper(nl, nr, mu, rho, afreq)
-        rnlist.append(rn)
+        # rn = neck_resistance(mu, gamma, thcon, cp, nl, nr, rho, afreq)  # neck resitance (kg/s)
+        rn = neck_resistance_paper(nl, nr, mu, rho, afreq)
+        rnlist.append(rn / 1e5)
 
         s = spring_stiffness(rho, c, sn, vol)           # spring stiffness
         zhr = hr_impedance(afreq, m, rn, s, sn)